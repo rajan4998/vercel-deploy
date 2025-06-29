ecae81cc6a19fb557f8345462ed9db12e64d35ae
Rajan Joshi <rajan4998@gmail.com>
2025-06-29 17:21:34 +0530

    db conn string updated

--- appconf.py
+++ appconf.py
@@ -1,14 +1,16 @@
 import mysql.connector
 from flask import Flask,request
+import psycopg2
 
 class AppConfig:
     def __init__(self):
-        self.conn = mysql.connector.connect(
-            host="ep-yellow-tree-a4zfakw4-pooler.us-east-1.aws.neon.tech",
-            user="neondb_owner",         
-            password="npg_pxHh86OMEAeB",  
-            database="neondb"
-        )
+        # self.conn = mysql.connector.connect(
+        #     host="ep-yellow-tree-a4zfakw4-pooler.us-east-1.aws.neon.tech",
+        #     user="neondb_owner",         
+        #     password="npg_pxHh86OMEAeB",  
+        #     database="neondb"
+        # )
+        self.conn = psycopg2.connect("postgres://0197bb82-647f-7268-9bb8-b4202cee5de2:fd07b846-19a8-461d-928c-a7adfe72fe45@us-west-2.db.thenile.dev/nile_yellow_garden", autocommit=True)
         self.app = None  # Placeholder for Flask app instance
         self.app = Flask(__name__)
         self.app.config['SECRET_KEY'] = '3o374eoxq0qiou4'  # Secret key for session management
